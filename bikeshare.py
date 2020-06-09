d1f25847012cdcdafcc3863238cfe6b6471c3223
rhn-github <richard.needham@bmw.de>
2020-06-09 14:25:47 +0200

    refactor: bikeshare.py - lines 6, 67 rename (decapitalise) CITY_DATA as city_data

--- bikeshare.py
+++ bikeshare.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import calendar as ca
 
-CITY_DATA = { 'chicago': 'chicago.csv',
+city_data = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
@@ -64,7 +64,7 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
     
-    df = pd.read_csv(CITY_DATA[city])
+    df = pd.read_csv(city_data[city])
          
     # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time']) 
